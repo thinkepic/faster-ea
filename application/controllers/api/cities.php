6498526261d74acf84ec5aafe184e14dc1afe247
Fadel Al Fayed <fadelalfayed27@gmail.com>
2022-05-20 01:30:23 +0700

    change table wilayah kabupaten

--- application/controllers/api/cities.php
+++ application/controllers/api/cities.php
@@ -6,16 +6,16 @@ class Cities extends MY_Controller {
     function index () {
 
         $this->db
-        ->from('cities');
+        ->from('tb_wilayah_kabupaten');
 
         if ($this->input->get('select2')) {
-            $this->db->select('name as id, name as text');
+            $this->db->select('nama as id, nama as text');
         }
         if ($this->input->get('q')) {
-            $this->db->like('name', $this->input->get('q'));
+            $this->db->like('nama', $this->input->get('q'));
         }
 
-        $this->db->order_by('name', 'asc');
+        $this->db->order_by('nama', 'asc');
 
         $response['result'] = $this->db->get()->result();
         $this->send_json($response);
